4f04063ab65f549f25f266f88c0a50bd60244e51
Braden <bradenfoxcroft@gmail.com>
2022-12-09 14:28:09 -0700

    Improved search: now sorted by relevance! (still exact matches, though).

--- stampede-buddy/stampede-buddy/Shared/SimpleSearch.cs
+++ stampede-buddy/stampede-buddy/Shared/SimpleSearch.cs
@@ -19,47 +19,52 @@ namespace stampede_buddy
         /// <returns>The list of valid results</returns>
         public static List<BaseEvent>? search(List<BaseEvent>? allEvents, String searchPhrase)
         {
-            // filters by whether the item matches the query. simple and clean.
-            List<BaseEvent>? result = allEvents?.Where(x => match(x,searchPhrase)).ToList();
+            // filters by whether the item matches the query.
+            // Sorts by how well it matches the query.
+            List<BaseEvent>? result = allEvents?.Where(x => MatchScore(x,searchPhrase) > 0).ToList();
+            // Sort by count
+            result = result?.OrderBy(x => -MatchScore(x,searchPhrase)).ToList();
             return result;
         }
 
         /// <summary>
-        /// Checks if an item matches a search query
+        /// Checks how many times the string appears in the event description.
         /// </summary>
         /// <param name="e">the event to check</param>
         /// <param name="sp">the search query</param>
         /// <returns>true if there is a match, false otherwise.</returns>
-        private static bool match(BaseEvent e, String sp)
+        private static int MatchScore(BaseEvent e, String sp)
         {
             // Handle cases where anything is null.
-            if (sp == null) return false;
-            if (e == null) return false;
-            if (e.EventName == null) return false;
-            if (e.BriefDescription == null) return false;
-            if (e.Location == null) return false;
-            if (e.Description== null) return false;
+            if (sp == null) return 0;
+            if (e == null) return 0;
+            if (e.EventName == null) return 0;
+            if (e.BriefDescription == null) return 0;
+            if (e.Location == null) return 0;
+            if (e.Description== null) return 0;
 
-            // Handle possible matches.
-            // I'm looking for possible matches only, cuz I'm lazy.
-            if (inStr(e.EventName,sp)) return true;
-            if (inStr(e.BriefDescription,sp)) return true;
-            if (inStr(e.Location,sp)) return true;
-            if (inStr(e.Description,sp)) return true;
-
-            // No match found.
-            return false;
+            // Handle possible matches
+            int score = 0;
+            score += CountInStr(e.EventName, sp) * 10; // event name matches get higher priority.
+            score += CountInStr(e.Location, sp) * 10; // location matches get higher priority.
+            score += CountInStr(e.BriefDescription, sp);
+            score += CountInStr(e.Description, sp);
+            
+            return score;
         }
 
-        /// <summary>
-        /// Checks if a string is in another string, in a case-insensitive way.
-        /// </summary>
-        /// <param name="haystack">The string to search</param>
-        /// <param name="needle">The string to look for</param>
-        /// <returns>True, if the needle is in the haystack</returns>
-        private static bool inStr(String haystack, String needle)
+        private static int CountInStr(String haystack, String needle)
         {
-            return haystack.ToLower().Contains(needle.ToLower());
+            String hs = haystack.ToLower();
+            String nd = needle.ToLower();
+            int count = 0;
+            int pos = 0;
+            while(hs.IndexOf(nd,pos) >= 0)
+            {
+                pos = hs.IndexOf(nd, pos) + 1;
+                count++;
+            }
+            return count;
         }
 }
 }
